21a2d05346f62187a7ed7db42f4f3ad2bcd47079
ermanzi <ermanzi@mit.edu>
2016-10-26 10:15:22 -0400

    aimd seq ack

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -23,10 +23,10 @@ unsigned int Controller::window_size( void )
   /* Default: fixed window size of 100 outstanding datagrams */
   unsigned int the_window_size = (unsigned int) cwnd;
 
-  if ( debug_ ) {
+//  if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
 	 << " window size is " << the_window_size << endl;
-  }
+//  }
 
   return the_window_size;
 }
@@ -59,29 +59,27 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 {
   /* Default: take no action */
 
-  if ( debug_ ) {
+//  if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
 	 << " received ack for datagram " << sequence_number_acked
 	 << " (send @ time " << send_timestamp_acked
 	 << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
 	 << endl;
-  }
+//  }
 
 
   /* Check if timeout exceeded */
 //  uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
 
-//  cerr << "Seqnum acked: " << sequence_number_acked <<", last acked: " << last_sequence_number_acked << endl;
+  cerr << "Seqnum acked: " << sequence_number_acked <<", last acked: " << last_sequence_number_acked << endl;
 //  if ((unsigned int) rtt > timeout_ms() ) {
   if (sequence_number_acked - last_sequence_number_acked > 1 ) {
-    cerr << "!!!Drop. Seqnum acked: " << sequence_number_acked
-    << ", last acked: " << last_sequence_number_acked
-    << ", window: " << window_size() << endl;
-//    if (num_acks_since_last_md > window_size()) {
-    cwnd = cwnd/factor;
-//    cerr << "acks since last md:" << num_acks_since_last_md << endl;
-    num_acks_since_last_md = 0;
-//    }
+    cerr << "!!!Drop. Seqnum acked: " << sequence_number_acked <<", last acked: " << last_sequence_number_acked << endl;
+    if (num_acks_since_last_md > window_size()) {
+      cwnd = cwnd/factor;
+      cerr << "acks since last md:" << num_acks_since_last_md << endl;
+      num_acks_since_last_md = 0;
+    }
   }
 //  cwnd = (cwnd >= max_wnd) ? max_wnd : cwnd+1;
   cwnd++;
