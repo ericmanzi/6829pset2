b17c865ecf80b7cf69d8a83c18876dbb5079eddb
ermanzi <ermanzi@mit.edu>
2016-10-26 10:08:13 -0400

    aimd seq ack

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -71,15 +71,15 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   /* Check if timeout exceeded */
 //  uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
 
-  cerr << "Seqnum acked: " << sequence_number_acked <<", last acked: " << last_sequence_number_acked << endl;
+//  cerr << "Seqnum acked: " << sequence_number_acked <<", last acked: " << last_sequence_number_acked << endl;
 //  if ((unsigned int) rtt > timeout_ms() ) {
   if (sequence_number_acked - last_sequence_number_acked > 1 ) {
     cerr << "!!!Drop. Seqnum acked: " << sequence_number_acked <<", last acked: " << last_sequence_number_acked << endl;
-    if (num_acks_since_last_md > window_size()) {
-      cwnd = cwnd/factor;
-      cerr << "acks since last md:" << num_acks_since_last_md << endl;
-      num_acks_since_last_md = 0;
-    }
+//    if (num_acks_since_last_md > window_size()) {
+    cwnd = cwnd/factor;
+//    cerr << "acks since last md:" << num_acks_since_last_md << endl;
+    num_acks_since_last_md = 0;
+//    }
   }
 //  cwnd = (cwnd >= max_wnd) ? max_wnd : cwnd+1;
   cwnd++;
