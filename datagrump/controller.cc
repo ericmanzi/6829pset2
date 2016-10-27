42a7468a39a6ad4dc0faab3090bb0ed881655788
ermanzi <ermanzi@mit.edu>
2016-10-27 02:34:44 -0400

    delay triggered - aggressive

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 float cwnd = 1;
 float ai = 2;
-float md_factor = 2;
+float md_factor = 1.1;
 unsigned int last_sequence_number_sent = 0;
 unsigned int last_sequence_number_acked = 0;
 unsigned int num_acks_til_next_md = 0;
@@ -93,5 +93,5 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return 1000;
+  return (unsigned int) ceil_threshold_factor * min_rtt;
 }
